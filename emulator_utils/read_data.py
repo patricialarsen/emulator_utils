48add7ea2f23a6a18d35a21e815ec53478e27a66
Tricia <prlarsenwork@gmail.com>
2022-04-15 15:28:09 -0500

    added correlation function read

--- emulator_utils/read_data.py
+++ emulator_utils/read_data.py
@@ -28,6 +28,31 @@ def readpowerspec(path):
         number of modes for power spectrum calculation in units of (Mpc/h)^3
 
     """
-    file_in = np.loadtxt(path,skiprows=1)
+    file_in = np.loadtxt(path, skiprows=1)
     return file_in[:,0], file_in[:,1], file_in[:,2], file_in[:,3]
 
+def readcorr(path):
+    """
+    Read in correlation function from a file path
+
+    Parameters
+    ----------
+    path: float
+       absolute path of correlation function file
+
+    Returns
+    -------
+    rlo: ndarray(float)
+        minimum bin distance in units of Mpc/h
+    rhi: ndarray(float)
+        maximum bin distance in units of Mpc/h
+    corr: ndarray(float)
+        correlation function
+    count: ndarray(int)
+        global count 
+    binsum: ndarray(int)
+        global bin sum 
+
+    """
+    file_in = np.loadtxt(path, skiprows=1)
+    return file_in[:,0], file_in[:,1], file_in[:,2], file_in[:,3], file_in[:,5]
