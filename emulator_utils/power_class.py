a2fbff8af4dd7dfa26307be62a1a6b5a49d3b836
Tricia <prlarsenwork@gmail.com>
2022-04-15 20:51:46 -0500

    added to power spectrum class

--- emulator_utils/power_class.py
+++ emulator_utils/power_class.py
@@ -5,17 +5,8 @@ class PowerSpectrum:
     Class defining useful power spectrum routines 
 
     """
-    def __init__(self, direc):
-        file_list = glob.glob(direc+'/*.pk.*')
-        assert(re.findall(r'\d+',direc)==[])
-        while i<len(file_list):
-            if (re.findall(r'\d+',file_list[i])==[]):
-                file_list = file_list.delete(i)
-            else:
-                i++
-        self.file_list = file_list 
-
-
+    def __init__(self):
+        return 
         
     def get_step_list_solo(self,file1)
         if re.findall(r'\d+',file1)==[]:
@@ -46,6 +37,21 @@ class PowerSpectrum:
         except:
             self.steps = map(self.get_step_list_solo,self.file_list)
             return self.steps
+        
+    @property
+    def set_data(self):
+        from read_data import readpowerspec
+        k, pk, err, npairs = [read_powerspec(file_i) for file_i in self.file_list]
+        self.k = k 
+        self.pk = pk
+        self.err = err
+        self.npairs = npairs
+        return 
 
 
+    @property
+    def set_conserved_quantities(self):
+        ""
+        from precompute_quantities import pk_ratio
+        self.pk_ratio = pk_ratio(self.k,self.pk,self.steps)
 
--- emulator_utils/precompute_quantities.py
+++ emulator_utils/precompute_quantities.py
@@ -5,4 +5,10 @@ Pre-compute conserved quantities
 
 """
 
+def pk_ratio(k_vals,pk_vals,steps):
+    nsteps = len(steps)
+    assert([k_vals[i]==k_vals[j] for i,j in range(nsteps)].all())
+    base_step = np.max(steps)
+    base_idx = np.where(steps==base_step)[0]
+    return pk_vals/pk_vals[base_idx]
 
