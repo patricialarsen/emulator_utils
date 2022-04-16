83181b808614f69895b916c1fe63485058e9bad9
Nesar Ramachandra <nramachandra@gpulogin1.lcrc.anl.gov>
2022-04-16 15:22:18 -0500

    2d things

--- emulator_utils/explainer.py
+++ emulator_utils/explainer.py
@@ -16,10 +16,18 @@ def local_explainer(model, training_data, test_data, input_names, output_names):
     shap_values = ex.shap_values(X_test[0:nb_samples, :])
 
 
+
+    explainer = shap.KernelExplainer(predictor, X_train, features = input_names, out_names = target_names)
+    shap_valuesKE = explainer.shap_values(X_test)
+
+
     p1 = shap.summary_plot(shap_valuesKE[0], X_test, feature_names = input_names, plot_type='violin')
     p2 = shap.summary_plot(shap_valuesKE[1], X_test, feature_names = input_names, plot_type='violin')
 
     p3 = shap.force_plot(ex.expected_value[out_id], shap_values[out_id][4], feature_names = input_names, out_names = target_names[out_id])
+    shap.force_plot(ex.expected_value[out_id], shap_values[out_id], feature_names = input_names, out_names = target_names[out_id])
+
+
 
     return p1, p2, p3
 
