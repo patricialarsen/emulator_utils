6b522dd21e7c0566191b0edd678fdab673b8c217
Tricia <prlarsenwork@gmail.com>
2022-04-16 13:16:13 -0500

    added LHC creation script

---
+++ emulator_utils/lhc.py
@@ -0,0 +1,32 @@
+import pyDOE
+import numpy as np
+
+def create_lhc(AllPara, num_evals, output_dir):
+    '''
+    Create a latin hypercube of the parameter range
+    Parameters
+    ----------
+    AllPara: ndarray(ndarray(float))
+       parameter options 
+    num_evals: int
+       number of points to evaluate at
+    output_dir: str
+       path to put outputs 
+    Returns
+    -------
+    params: ndarray(ndarray(float))
+       selected choices of parameters
+    '''
+    num_params = AllPara.shape[0]
+    lhd = pyDOE.lhs(num_params, samples=num_evals, criterion=None) # c cm corr m
+    idx = (lhd * num_evals).astype(int)
+
+    AllCombinations = np.zeros((num_evals, AllPara.shape[0]))
+
+    for i in range(AllPara.shape[0]):
+        AllCombinations[:, i] = AllPara[i][idx[:, i]]
+    params = AllCombinations
+
+    np.savetxt(output_dir + '/lhc_'+str(num_evals)+'_'+str(num_params)+'.txt', AllCombinations)
+    return params
+
