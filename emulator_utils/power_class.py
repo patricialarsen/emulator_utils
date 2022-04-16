37c8fd76a63eccbbdfbd0f854632a3742085c512
Tricia <prlarsenwork@gmail.com>
2022-04-15 22:01:32 -0500

    adding to pk test and fixing more bugs

--- emulator_utils/power_class.py
+++ emulator_utils/power_class.py
@@ -1,4 +1,5 @@
 import glob
+import re 
 
 class PowerSpectrum:
     """ 
@@ -14,33 +15,34 @@ class PowerSpectrum:
         else:
             return re.findall(r'\d+',file1)[-1]
 
-    @property 
-    def file_list(self, direc):
-        try:
-            return self.file_list
-        except:
-            file_list = glob.glob(direc+'/*.pk.*')
-            assert(re.findall(r'\d+',direc)==[])
-            while i<len(file_list):
-                if (re.findall(r'\d+',file_list[i])==[]):
-                    file_list = file_list.delete(i)
-                else:
-                    i+=1
-            self.file_list = file_list
-            return file_list 
+    def set_file_list(self, direc):
+        file_list = glob.glob(direc+'/*.pk.*')
+        assert(re.findall(r'\d+',direc)==[])
+        i=0
+        while i<len(file_list):
+            if (re.findall(r'\d+',file_list[i])[-1]=='000'):
+                file_list.pop(i)
+                print('deleted')
+            else:
+                print(re.findall(r'\d+',file_list[i])[-1])
+                i+=1
+        self.file_list = file_list
+        return file_list 
 
-    @property
-    def steps(self):
+    def set_steps(self):
         """ndarray(int) simulation steps with power spectra"""
         try:
             return self.steps
         except:
-            self.steps = map(self.get_step_list_solo,self.file_list)
+            self.steps = list(map(self.get_step_list_solo,self.file_list))
             return self.steps
         
     def set_data(self):
-        from read_data import readpowerspec
-        k, pk, err, npairs = [read_powerspec(file_i) for file_i in self.file_list]
+        from emulator_utils.read_data import readpowerspec
+        k=[];pk=[];err=[];npairs=[]
+        for file_i in self.file_list:
+            k_tmp, pk_tmp, err_tmp, npairs_tmp = readpowerspec(file_i)
+            k.append(k_tmp);pk.append(pk_tmp);err.append(err_tmp);npairs.append(npairs_tmp)
         self.k = k 
         self.pk = pk
         self.err = err
--- test/test_pk_class.py
+++ test/test_pk_class.py
@@ -11,4 +11,25 @@ def test_initialize():
     except:
         assert False
 
+def test_filelist():
+    try:
+        powerspec = PowerSpectrum()
+        file_list = powerspec.set_file_list(direc='test/data/powerspec_LJ')
+        print(file_list)
+        print(len(file_list))
+        assert len(file_list)==73
+    except:
+        assert False
 
+def test_step():
+    powerspec = PowerSpectrum()
+    file_list = powerspec.set_file_list(direc='test/data/powerspec_LJ')
+    steps = powerspec.set_steps()
+    assert(len(steps)==73)
+
+def test_data():
+    powerspec = PowerSpectrum()
+    powerspec.set_file_list(direc='test/data/powerspec_LJ')
+    powerspec.set_steps()
+    powerspec.set_data()
+    assert (powerspec.k!=None)
