ecc5659d0b2be6d919e1105a89fdcbd7b58cf81d
Nesar Ramachandra <nramachandra@gpu1.lcrc.anl.gov>
2022-04-20 16:28:37 -0500

    dropout added

--- emulator_utils/surrogates.py
+++ emulator_utils/surrogates.py
@@ -18,6 +18,7 @@ def simple_mlp(input_shape, output_shape, hidden_dims):
     TO-DO: add options for changing loss, metrics, and optimizer
 
     '''
+    p_dropout = 0.2
 
     model = Sequential()
 
@@ -25,6 +26,8 @@ def simple_mlp(input_shape, output_shape, hidden_dims):
 
     for hidden_shape in hidden_dims[1:]:
         model.add(Dense(hidden_shape, activation='relu', kernel_initializer='he_normal'))
+
+    model.add(Dropout(p_dropout))
 
     model.add(Dense(output_shape, activation='linear'))
     print(model.summary())
